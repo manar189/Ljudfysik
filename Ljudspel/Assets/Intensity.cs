c67a6496afc0039494a54dfd3dd64f29d4978915
Martin Hag <martin-hag@hotmail.com>
2019-09-19 17:44:48 +0200

    Inital commit
    Created unity project

---
+++ Ljudspel/Assets/Intensity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intensity : MonoBehaviour {
+    float intensity;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        intensity = SoundRecorder.intensity;
+        if (intensity > -1000 && intensity < 1000)
+            transform.position = new Vector3(intensity/1.0f, 0, 0);
+	}
+    
+}
---
+++ Ljudspel/Assets/SoundRecorder.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent ( typeof(AudioSource))]
+public class SoundRecorder : MonoBehaviour {
+
+    AudioSource audioSource;
+
+    //Microphone input
+    public AudioClip audioClip;
+    public bool useMicrophone = false;
+    public string selectedDevice;
+
+    public  float[] samples = new float[512];
+    public float[] samplesDB = new float[128];
+    public static float intensity = 0;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+       
+
+        // Prints all microphones found in the device.
+        for (int i = 0; i < Microphone.devices.Length; i++)
+        {
+            Debug.Log(Microphone.devices[i].ToString());
+        }
+
+        if (useMicrophone)
+        {
+            if (Microphone.devices.Length > 0)
+            {
+                selectedDevice = Microphone.devices[0].ToString();
+                audioSource.clip = Microphone.Start(selectedDevice, true, 10, AudioSettings.outputSampleRate);
+            }
+            else
+            {
+                useMicrophone = false;
+            }
+        }
+        if (!useMicrophone)
+        {
+            audioSource.clip = audioClip;
+        }
+        audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetSpectrumAudioSource();
+	}
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Hamming);
+
+        audioSource.GetOutputData(samplesDB, 0);
+
+        float sum = 0;
+        for (int i = 0; i < samplesDB.Length; i++)
+        {
+            sum += samplesDB[i] * samplesDB[i];
+        }
+        float RMS = sum / samplesDB.Length; //Root-Mean-Squared.
+        float RefValue = samplesDB.Length / (float)AudioSettings.outputSampleRate;
+        intensity = 20 * Mathf.Log10(RMS / RefValue);
+    }
+}
