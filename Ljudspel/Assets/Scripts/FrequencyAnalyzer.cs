23a7859f9ff9ad662e909f1a7068d828c82ffc50
manar189 <mkjaronsson@gmail.com>
2019-09-25 21:33:49 +0200

    Actually not crashing code. Almost able to detect pitch but needs finishing touches.

--- Ljudspel/Assets/Scripts/FrequencyAnalyzer.cs
+++ Ljudspel/Assets/Scripts/FrequencyAnalyzer.cs
@@ -4,34 +4,72 @@ using UnityEngine;
 
 public class FrequencyAnalyzer : MonoBehaviour
 {
-    
-    int nSamples = 1024;
-    float refValue = 0.1;
-    float threshold = 0.02;
-    float rmsValue;
-    float dbValue;
-    float pitchValue;
+
+    public const int qSamples = 1024;
+    public float refValue = 0.1f; // RMS value for 0 dB
+    public float threshold = 0.02f;  // minimum amplitude to extract pitch
+    public float rmsValue;   // sound level - RMS
+    public float dbValue;    // sound level - dB
+    public double pitchValue;    // sound pitch - Hz
 
     private float[] samples;
     private float[] spectrum;
     private float fSample;
+    AudioSource audioSource;
 
-    void Start()
-    {
-        samples = new float[nSamples];
-        spectrum = new float[nSamples];
-        fSample = AudioSettings.outputSampleRate;
-    }
 
     void AnalyzeSound()
     {
+        //audioSource.GetSpectrumData(samples, 0, FFTWindow.Hamming);
+        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        float maxV = 0.0f;
+        int maxN = 0;
+
         
+        for (int i = 0; i<qSamples; ++i)
+        { // find max 
+            if (spectrum[i] > maxV && spectrum[i] > threshold)
+            {
+                maxV = spectrum[i];
+                maxN = i; // maxN is the index of max
+                //Debug.Log("maxV: "+maxV);
+                //Debug.Log("maxN: "+maxN);
+            }
+        }
+
+        double freqN = maxN; // pass the index to a float variable
+        if (maxN > 0 && maxN < qSamples - 1)
+        { // interpolate index using neighbours
+            double dL = spectrum[maxN - 1] / spectrum[maxN];
+            double dR = spectrum[maxN + 1] / spectrum[maxN];
+            freqN += 0.5 * (dR * dR - dL * dL);
+        }
+
+        pitchValue = freqN * (fSample / 2) / qSamples; // convert index to frequency
+
+        Debug.Log("PitchValue: " + pitchValue);
+
     }
+
+
+
+    void Start()
+    {
+        samples = new float[qSamples];
+        spectrum = new float[qSamples];
+        fSample = AudioSettings.outputSampleRate;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+
 
 
     void Update()
     {
-        Console::Write("testing123");
+        AnalyzeSound();
+        
     }
     
 
