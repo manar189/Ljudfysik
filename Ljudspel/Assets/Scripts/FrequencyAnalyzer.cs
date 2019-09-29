e54d3dd0acef762220db4c9e6ac1c003e02ad838
manar189 <mkjaronsson@gmail.com>
2019-09-29 20:34:47 +0200

    Working frequency analysis. Gets the base pitch from a harmonic product spectrum.

--- Ljudspel/Assets/Scripts/FrequencyAnalyzer.cs
+++ Ljudspel/Assets/Scripts/FrequencyAnalyzer.cs
@@ -4,135 +4,75 @@ using UnityEngine;
 
 public class FrequencyAnalyzer : MonoBehaviour
 {
+    public GameObject cursor;
 
-    public const int qSamples = 1024;
-    public float refValue = 0.1f; // RMS value for 0 dB
-    public float threshold = 0.02f;  // minimum amplitude to extract pitch
-    public float rmsValue;   // sound level - RMS
-    public float dbValue;    // sound level - dB
-    public double pitchValue;    // sound pitch - Hz
+    public const int qSamples = 8192;
+    public float threshold = 0.1f;  // minimum amplitude to extract pitch
 
-    private float[] samples;
+    public float pitchValue;    // sound pitch - Hz
+
     private float[] spectrum;
     private float fSample;
     AudioSource audioSource;
 
 
-    void AnalyzeSound()
+    float getFrequency()
     {
-        //audioSource.GetSpectrumData(samples, 0, FFTWindow.Hamming);
+      
         audioSource.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
 
-        float maxV = 0.0f;
-        int maxN = 0;
+        // Creates harmonic product spectrum
+        float[] hps = spectrum; 
+        for (int i = 0; i < qSamples/2; ++i)
+        {
+            hps[i] += spectrum[i * 2];
 
-        
-        for (int i = 0; i<qSamples; ++i)
-        { // find max 
-            if (spectrum[i] > maxV && spectrum[i] > threshold)
+            if (i < qSamples / 3)
             {
-                maxV = spectrum[i];
-                maxN = i; // maxN is the index of max
-                //Debug.Log("maxV: "+maxV);
-                //Debug.Log("maxN: "+maxN);
+                hps[i] += +spectrum[i * 3];
+            }
+
+            if (i < qSamples / 4)
+            {
+                hps[i] += +spectrum[i * 4];
             }
         }
 
-        double freqN = maxN; // pass the index to a float variable
-        if (maxN > 0 && maxN < qSamples - 1)
-        { // interpolate index using neighbours
-            double dL = spectrum[maxN - 1] / spectrum[maxN];
-            double dR = spectrum[maxN + 1] / spectrum[maxN];
-            freqN += 0.5 * (dR * dR - dL * dL);
+        // Finds the index of strongest base pitch
+        float maxV = 0.0f;
+        int maxN = 0;
+        for (int i = 0; i<qSamples; i++)
+        { 
+            if (hps[i] > maxV && hps[i] > threshold)
+            {
+                maxV = hps[i];
+                maxN = i;
+            }
         }
 
-        pitchValue = freqN * (fSample / 2) / qSamples; // convert index to frequency
+        // Interpolate index using neighbours
+        float freqN = maxN;
+        if (maxN > 0 && maxN < qSamples - 1)
+        { 
+            float dL = hps[maxN - 1] / hps[maxN];
+            float dR = hps[maxN + 1] / hps[maxN];
+            freqN += 0.5f * (dR * dR - dL * dL);
+        }
 
-        Debug.Log("PitchValue: " + pitchValue);
-
+        // Returns frequency
+        return freqN * (fSample / 2) / qSamples;
     }
-
-
 
     void Start()
     {
-        samples = new float[qSamples];
         spectrum = new float[qSamples];
         fSample = AudioSettings.outputSampleRate;
         audioSource = GetComponent<AudioSource>();
     }
 
-
-
-
-
     void Update()
     {
-        AnalyzeSound();
-        
+        pitchValue = getFrequency();
+        Debug.Log(pitchValue);
     }
-    
-
 }
-
-/*
- var qSamples: int = 1024;  // array size
- var refValue: float = 0.1; // RMS value for 0 dB
- var threshold = 0.02;      // minimum amplitude to extract pitch
- var rmsValue: float;   // sound level - RMS
- var dbValue: float;    // sound level - dB
- var pitchValue: float; // sound pitch - Hz
- 
- private var samples: float[]; // audio samples
- private var spectrum: float[]; // audio spectrum
- private var fSample: float;
- 
- function Start () {
-     samples = new float[qSamples];
-     spectrum = new float[qSamples];
-     fSample = AudioSettings.outputSampleRate;
- }
- 
- function AnalyzeSound(){
-     audio.GetOutputData(samples, 0); // fill array with samples
-     var i: int;
-     var sum: float = 0;
-     for (i=0; i < qSamples; i++){
-         sum += samples[i]*samples[i]; // sum squared samples
-     }
-     rmsValue = Mathf.Sqrt(sum/qSamples); // rms = square root of average
-     dbValue = 20*Mathf.Log10(rmsValue/refValue); // calculate dB
-     if (dbValue < -160) dbValue = -160; // clamp it to -160dB min
-     // get sound spectrum
-     audio.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
-     var maxV: float = 0;
-     var maxN: int = 0;
-     for (i=0; i < qSamples; i++){ // find max 
-         if (spectrum[i] > maxV && spectrum[i] > threshold){
-             maxV = spectrum[i];
-             maxN = i; // maxN is the index of max
-         }
-     }
-     var freqN: float = maxN; // pass the index to a float variable
-     if (maxN > 0 && maxN < qSamples-1){ // interpolate index using neighbours
-         var dL = spectrum[maxN-1]/spectrum[maxN];
-         var dR = spectrum[maxN+1]/spectrum[maxN];
-         freqN += 0.5*(dR*dR - dL*dL);
-     }
-     pitchValue = freqN*(fSample/2)/qSamples; // convert index to frequency
- }
- 
- var display: GUIText; // drag a GUIText here to show results
- 
- function Update () {
-     if (Input.GetKeyDown("p")){
-         audio.Play();
-     }
-     AnalyzeSound();
-     if (display){ 
-         display.text = "RMS: "+rmsValue.ToString("F2")+
-         " ("+dbValue.ToString("F1")+" dB)\n"+
-         "Pitch: "+pitchValue.ToString("F0")+" Hz";
-     }
- }
-*/
